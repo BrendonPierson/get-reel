ec0fab7e824f83b2dcd2c754c5d79ec0072e5fda
Daniel Hodges <danielhodges@Daniels-MacBook-Pro-2.local>
2015-08-11 10:25:00 -0500

    created main.js

---
+++ javascripts/main.js
@@ -0,0 +1,22 @@
+requirejs.config({
+  baseUrl: './javascripts',
+  paths: {
+    'jquery' : '../bower_components/jquery/dist/jquery.min',
+    'firebase' : '../bower_components/firebase/firebase',
+    'hbs' : '../bower_components/require-handlebars-plugin/hbs',
+    'bootstrap' : '../bower_components/bootstrap/dist/js/bootstrap.min',
+    'lodash' : '../bower_components/lodash/lodash.min'
+  },
+  shim: {
+    'bootstrap': ['jquery'],
+    'material': ['bootstrap'],
+    'firebase': {
+      exports: 'Firebase'
+    }
+  }
+});
+
+requirejs(
+  ["firebase", "jquery","lodash", "hbs", "bootstrap"],
+  function(_firebase, $, _, Handlebars, bootstrap) {
+});
