4ee9bbfed1ad6f392a377cfd70987102b23d195b
TGriffey <tgriffey9389@gmail.com>
2015-08-12 10:15:53 -0500

    Added watched.js module, watched feature functioning.

--- javascripts/main.js
+++ javascripts/main.js
@@ -17,50 +17,70 @@ requirejs.config({
 });
 
 requirejs(
-  ["firebase", "jquery","lodash", "hbs", "bootstrap"],
-  function(_firebase, $, _, Handlebars, bootstrap) {
+  ["firebase", "jquery","lodash", "hbs", "bootstrap", "watched"],
+  function(_firebase, $, _, Handlebars, bootstrap, watched) {
   var myFirebaseRef = new Firebase('https://get-reel.firebaseio.com/');
 
-  myFirebaseRef.on("value", function(snapshot) {
-    // console.log(snapshot.val());
-    var allMovies = snapshot.val();
-    var allMoviesArray = [];
-     // Convert Firebase's object of objects into an array of objects
-    for (var key in allMovies) {
-      allMoviesArray[allMoviesArray.length] = allMovies[key];
-    }
-    var allMoviesObject = {movies: allMoviesArray};
-    // console.log(allMoviesObject);
+    var allMovies = {};
+    var allMoviesObject = {};
+    // Firebase snapshot function to reload data on change.
+    myFirebaseRef.on("value", function(snapshot) {
+      // console.log(snapshot.val());
+      allMovies = snapshot.val();
+      var allMoviesArray = [];
+       // Convert Firebase's object of objects into an array of objects
+      for (var key in allMovies) {
+        allMoviesArray[allMoviesArray.length] = allMovies[key];
+      }
+      allMoviesObject = {movies: allMoviesArray};
+      // console.log(allMoviesObject);
 
-    require(['hbs!../templates/movies'], function(template) {
-      $(".row").html(template(allMoviesArray));
+      require(['hbs!../templates/movies'], function(template) {
+        $(".row").html(template(allMoviesArray));
+      });
     });
-  });
   
-// On clicking "Spin the Reel":
-$('#movie-search').click(function () {
+    // On clicking "Spin the Reel": (consider moving this function to module?)
+    $('#movie-search').click(function () {
+        // Close form: (possible modification - on click, hidden div displays with the movie info asking to confirm the selection.)
 
-    // Capture user input
-    var titleInput = $('#title-input').val();
-    var yearInput = $('#year-input').val(); 
+        // Capture user input
+        var titleInput = $('#title-input').val();
+        var yearInput = $('#year-input').val(); 
 
-    // Run ajax call to get data
-    $.ajax({
-        url: 'http://www.omdbapi.com/?t=' + titleInput + '&y=' + yearInput + '&plot=short&r=json'
-    }).done(function (data) {
-        console.log(data);
-        // // display data in thumbnail format, prompting the user to "Add" or "Cancel" to confirm the addition
-        // //Parse JSON into javascript object
-        // var movieResult = JSON.parse(data);
-        // //
-        
-        myFirebaseRef.push(data);
+        // Run ajax call to get data
+        $.ajax({
+            url: 'http://www.omdbapi.com/?t=' + titleInput + '&y=' + yearInput + '&plot=short&r=json'
+        }).done(function (data) {
+            data.watched = false;
+            data.rating = "Not rated";
+            console.log(data);
+            myFirebaseRef.push(data);
+          });
+    });
 
-        });
+      $(document).on('click', '.watched', function (e) {
+        e.preventDefault();
+        // Grab the key from Firebase and change key watched to true
 
-        });
+        var allTitles = [];
+        var watchedMovie = $(this).siblings('.caption').children('#movieName').text();
+        allTitles = _.pluck(allMovies, 'Title');
+         for (var i = 0; i < allTitles.length; i++) {
+           if (allTitles[i] === watchedMovie) {
+             watchedProperty = _.findKey(allMovies, {'Title': watchedMovie});
+             console.log(watchedProperty);
+           }
+          }
+        // Run movieWatched function from watched.js module.
+        watched.movieWatched(this, watchedProperty, true);
+      });
 
-
+      $(document).on('click', '.submit-rating', function (e) {
+        e.preventDefault();
+        // Create submit-rating module to add rating to Firebase (see above click function)
+          
+      });
 
     });
 
