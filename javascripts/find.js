c703ae17b8c606064db3f10bdac8bd5f6aaa039a
Jesse Turner <jesseturner@comcast.net>
2015-08-13 13:45:49 -0500

    modal html and find.js start

---
+++ javascripts/find.js
@@ -0,0 +1,11 @@
+define(["jquery", "firebase"], function ($, _firebase) {
+  $(document).on("click", '#add-movie', function() {
+    var userInput = $('#userInput').val();
+    $.ajax({
+      url: 'http://www.omdbapi.com/?s=' + titleInput,
+      method: 'GET'
+    }).done(function (data) {
+        console.log(data);
+    });
+  }//end of find click
+}//end of module
