abbf99525be1b6df157021fe3a7a80f9eec1c50a
Jesse Turner <jesseturner@comcast.net>
2015-08-13 14:49:31 -0500

    push to remove errors

--- javascripts/find.js
+++ javascripts/find.js
@@ -1,11 +1,22 @@
-define(["jquery", "firebase"], function ($, _firebase) {
-  $(document).on("click", '#add-movie', function() {
-    var userInput = $('#userInput').val();
-    $.ajax({
-      url: 'http://www.omdbapi.com/?s=' + titleInput,
-      method: 'GET'
-    }).done(function (data) {
-        console.log(data);
-    });//end of ajax done
-  });//end of find click
-});//end of module
+// define(["jquery", "firebase"], function ($, _firebase) {
+//   var imdbId = [];
+//   var posterLinks = [];
+//   $(document).on("click", '#add-movie', function() {
+//     var userInput = $('#userInput').val();
+//     $.ajax({
+//       url: 'http://www.omdbapi.com/?s=' + userInput,
+//       method: 'GET'
+//     }).done(function (data) {
+//       // for (var i in data.Search) {
+//       //   imdbId.push(data.Search[i].imdbID);
+//       // }
+//       // for (var i in imdbId) {
+//       //   $.ajax({
+//       //     url: 'http://www.omdbapi.com/?i=' + i + '&plot=short&r=json',
+//       //     method: 'GET'
+//       // }).done(function (data)) {
+//       //   console.log(data);
+//       }//end of ajax poster
+//     // });//end of ajax id done
+//   // });//end of find click
+// });//end of module
